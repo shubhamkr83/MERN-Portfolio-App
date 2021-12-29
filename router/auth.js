3c3335d03955cdfcd98b9255d844e5080f366f44
Shubham Kr <72254047+shubhamkr83@users.noreply.github.com>
2021-12-29 19:09:46 +0530

    deleted default path

--- router/auth.js
+++ router/auth.js
@@ -6,9 +6,6 @@ const app = express();
 require('../db/conn');
 const User = require('../model/userSchema');
 
-router.get('/', (req, res) => {
-    res.send(`Hello world from the server router js`);
-});
 
 
 //---------------- Contact Us ----------------
